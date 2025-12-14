27fe994b0a142a2fe534299ed5c6268e2e1c323b
busy... <110780871+OmonR@users.noreply.github.com>
2025-12-14 14:55:50 +0300

    Add SQL view, CTE, and temporary table for reports
    This SQL script creates a view for active session details, a CTE for long trips, and a temporary table for driver reports.

---
+++ Progect_SQL/04_virtualization.sql
@@ -0,0 +1,37 @@
+--VIEW--
+-- скрывает JOIN'ы и вычисляет длительность поездки.
+CREATE OR REPLACE VIEW v_active_sessions_details AS
+SELECT 
+    s.id AS session_id,
+    u.full_name AS driver_name,
+    c.plate,
+    c.make || ' ' || c.model AS car_model,
+    s.started_at,
+    (NOW() - s.started_at) AS duration_interval
+FROM car_sessions s
+JOIN users u ON s.user_id = u.id
+JOIN cars c ON s.car_id = c.id
+WHERE s.status = 'open';
+
+--CTE--
+-- поиск машин, которые проехали больше 100 км за поездку
+WITH long_trips AS (
+    SELECT 
+        car_id, 
+        (end_odo - start_odo) as distance
+    FROM car_sessions
+    WHERE status = 'closed'
+)
+SELECT 
+    c.plate, 
+    lt.distance 
+FROM long_trips lt
+JOIN cars c ON lt.car_id = c.id
+WHERE lt.distance > 100;
+
+--TEMP TABLE--
+-- отчет, который нужен только в рамках текущей сессии администратора
+CREATE TEMP TABLE temp_driver_report AS
+SELECT user_id, COUNT(*) as trip_count 
+FROM car_sessions 
+GROUP BY user_id;
